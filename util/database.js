f903db051f7f144c406113a6972bfc347b025263
Ramlen97 <rameshwarlenka97@gmail.com>
2023-03-07 20:42:13 +0530

    backendready

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense-tracker',process.env.MYSQL_USER,process.env.MYSQL_USER_PASSWORD,{
-    dialect:"mysql",
-    host:"localhost"
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+    dialect:'mysql',
+    host:process.env.DB_HOST
 });
 
 module.exports=sequelize;
